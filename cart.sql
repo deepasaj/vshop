074bf785a2d47a9768afef557b90990c6ed913d7
Aruna Shanmugakumar <arunash@thoughtworks.com>
2017-05-06 12:38:41 +0530

    DB changes for product + using heroku db

--- cart.sql
+++ cart.sql
@@ -3,7 +3,18 @@ CREATE DATABASE shop;
 
 \c shop;
 
+CREATE TABLE products(
+  id integer PRIMARY KEY,
+  name VARCHAR(20),
+  description VARCHAR(20),
+  price decimal
+);
+INSERT INTO products(id, name, description, price) values(1, 'Calvin Klien', 'Aqua women perfume', 4000);
+INSERT INTO products(id, name, description, price) values(2, 'Roberto Cavalli', '75ml Women fragrance', 3000);
+INSERT INTO products(id, name, description, price) values(3, 'Giorgio Armani', '75ml Women fragrance', 5000);
+
 CREATE TABLE cartitems (
   id SERIAL PRIMARY KEY,
-  product_id VARCHAR
+  user_id VARCHAR(20),
+  product_id VARCHAR(20)
 );
