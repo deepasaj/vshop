d473f9e2499519f9aa884cd1d197c275df200630
Sajani <deepasaj@thoughtworks.com>
2017-05-06 10:53:40 +0530

    Opaque Hotspots for images. Popup only on product view

--- VR_View/website/vrview/examples/hotspots/index.js
+++ VR_View/website/vrview/examples/hotspots/index.js
@@ -16,95 +16,131 @@ var vrView;
 
 // All the scenes for the experience
 var scenes = {
-    temp: {
-        image: 'temp.jpg',
-        preview: 'temp.jpg',
-        hotspots: {
-            room1: {
-                pitch: 0,
-                yaw: 38,
-                radius: 0.05,
-                distance: 1
-            },
-            room2: {
-                pitch: 0,
-                yaw: 12,
-                radius: 0.05,
-                distance: 1
-            },
-            room3: {
-                pitch: 0,
-                yaw: -64,
-                radius: 0.05,
-                distance: 1
-            },
-            room4: {
-                pitch: 0,
-                yaw: -90,
-                radius: 0.05,
-                distance: 1
-            },
-            room5: {
-                pitch: 0,
-                yaw: -167,
-                radius: 0.05,
-                distance: 1
-            },
-            room6: {
-                pitch: 0,
-                yaw: -245,
-                radius: 0.05,
-                distance: 1
-            }
-        }
-    },
-    room1: {
-        image: 'room1.jpg',
-        preview: 'room1.jpg',
-        hotspots: {
-            temp: {
-                pitch: 0,
-                yaw: 0,
-                radius: 0.05,
-                distance: 1
-            }
-        }
+  room: {
+    image: 'room.jpg',
+    preview: 'room.jpg',
+    hotspots: {
+      room1: {
+        pitch: 0,
+        yaw: 38,
+        radius: 0.05,
+        distance: 1
+      },
+      room2: {
+        pitch: 0,
+        yaw: 12,
+        radius: 0.05,
+        distance: 1
+      },
+      room3: {
+        pitch: 0,
+        yaw: -64,
+        radius: 0.05,
+        distance: 1
+      },
+      room4: {
+        pitch: 0,
+        yaw: -90,
+        radius: 0.05,
+        distance: 1
+      },
+      room5: {
+        pitch: 0,
+        yaw: -167,
+        radius: 0.05,
+        distance: 1
+      },
+      room6: {
+        pitch: 0,
+        yaw: -245,
+        radius: 0.05,
+        distance: 1
+      },
+      item1_view: {
+        pitch: 31,
+        yaw: -300.5,
+        radius: 0.03,
+        distance: 1
+      },
+      item1_cart: {
+        pitch: 15,
+        yaw: -297.5,
+        radius: 0.03,
+        distance: 1
+      },
+      item2_view: {
+        pitch: -5,
+        yaw: -277.5,
+        radius: 0.03,
+        distance: 1
+      },
+      item2_cart: {
+        pitch: -5,
+        yaw: -282.5,
+        radius: 0.03,
+        distance: 1
+      },
+      item3_view: {
+        pitch: -65,
+        yaw: -252.5,
+        radius: 0.03,
+        distance: 1
+      },
+      item3_cart: {
+        pitch: -39,
+        yaw: -260.5,
+        radius: 0.03,
+        distance: 1
+      }
     }
+  },
+  room1: {
+    image: 'room1.jpg',
+    preview: 'room1.jpg',
+    hotspots: {
+      room: {
+        pitch: 0,
+        yaw: 0,
+        radius: 0.05,
+        distance: 1
+      }
+    }
+  }
 };
 
 function onLoad() {
-    vrView = new VRView.Player('#vrview', {
-        image: 'blank.png',
-        preview: 'blank.png',
-        is_stereo: true,
-        is_autopan_off: true
-    });
+  vrView = new VRView.Player('#vrview', {
+    image: 'blank.png',
+    preview: 'blank.png',
+    is_stereo: true,
+    is_autopan_off: true
+  });
 
-    vrView.on('ready', onVRViewReady);
-    vrView.on('modechange', onModeChange);
-    vrView.on('click', onHotspotClick);
-    vrView.on('error', onVRViewError);
+  vrView.on('ready', onVRViewReady);
+  vrView.on('modechange', onModeChange);
+  vrView.on('click', onHotspotClick);
+  vrView.on('error', onVRViewError);
 }
 
 function onVRViewReady(e) {
-    console.log('onVRViewReady');
-    //var name = getParameterByName('name');
-    loadScene("temp");
+  console.log('onVRViewReady');
+  //var name = getParameterByName('name');
+  loadScene("room");
 }
 
 function onModeChange(e) {
-    console.log('onModeChange', e.mode);
+  console.log('onModeChange', e.mode);
 }
 
 function renderProductInfoPopup(productId) {
-    var iframe = document.getElementsByTagName('iframe')[0].contentDocument;
-    var popup = iframe.getElementsByClassName('dialog')[0];
-    var title = iframe.getElementsByClassName('title')[0];
-    var description = iframe.getElementsByClassName('message')[0];
-    // get product info from db
-    popup.style.display = 'block';
-    title.textContent = 'abdh';
-    description.textContent = 'mhvf';
+  var iframe = document.getElementsByTagName('iframe')[0].contentDocument;
+  var popup = iframe.getElementsByClassName('dialog')[0];
+  var title = iframe.getElementsByClassName('title')[0];
+  var description = iframe.getElementsByClassName('message')[0];
+  // get product info from db
+  popup.style.display = 'block';
+  title.textContent = 'abdh';
+  description.textContent = 'mhvf';
 }
 
 function goToNextRoom(roomId) {
@@ -112,45 +148,45 @@ function goToNextRoom(roomId) {
 }
 
 function onHotspotClick(e) {
-    var id = e.id;
+  var id = e.id;
     console.log('onHotspotClick', id);
-    if (id && id in scenes) {
-        goToNextRoom(id);
-    } else if (id) {
-        renderProductInfoPopup(id);
-    }
+  if (id && id in scenes && id.includes('room')) {
+    goToNextRoom(id);
+  } else if (id && id.includes('item') && id.includes('view')) {
+    renderProductInfoPopup(id);
+  }
 }
 
 
 function loadScene(id) {
-    console.log('loadScene', id);
+  console.log('loadScene', id);
 
-    // Set the image
-    vrView.setContent({
-        image: scenes[id].image,
-        preview: scenes[id].preview,
-        is_stereo: true,
-        is_autopan_off: true
+  // Set the image
+  vrView.setContent({
+    image: scenes[id].image,
+    preview: scenes[id].preview,
+    is_stereo: true,
+    is_autopan_off: true
+  });
+
+  // Add all the hotspots for the scene
+  var newScene = scenes[id];
+  var sceneHotspots = Object.keys(newScene.hotspots);
+  for (var i = 0; i < sceneHotspots.length; i++) {
+    var hotspotKey = sceneHotspots[i];
+    var hotspot = newScene.hotspots[hotspotKey];
+
+    vrView.addHotspot(hotspotKey, {
+      pitch: hotspot.pitch,
+      yaw: hotspot.yaw,
+      radius: hotspot.radius,
+      distance: hotspot.distance
     });
-
-    // Add all the hotspots for the scene
-    var newScene = scenes[id];
-    var sceneHotspots = Object.keys(newScene.hotspots);
-    for (var i = 0; i < sceneHotspots.length; i++) {
-        var hotspotKey = sceneHotspots[i];
-        var hotspot = newScene.hotspots[hotspotKey];
-
-        vrView.addHotspot(hotspotKey, {
-            pitch: hotspot.pitch,
-            yaw: hotspot.yaw,
-            radius: hotspot.radius,
-            distance: hotspot.distance
-        });
-    }
+  }
 }
 
 function onVRViewError(e) {
-    console.log('Error! %s', e.message);
+  console.log('Error! %s', e.message);
 }
 
 function getParameterByName(name) {
