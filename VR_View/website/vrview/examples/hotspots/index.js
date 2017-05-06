8bfa88abd8ad6b1e72f6d5be2d7d54b1b24dbd2e
Sajani <deepasaj@thoughtworks.com>
2017-05-07 03:23:03 +0530

    Revert "Mokka trial"
    This reverts commit 5d1f532ff76acaeb9d9497074070badb7a99d9dc.

--- VR_View/website/vrview/examples/hotspots/index.js
+++ VR_View/website/vrview/examples/hotspots/index.js
@@ -466,9 +466,8 @@ function onHotspotClick(e) {
 
 function init() {
     var iframe = document.getElementsByTagName('iframe')[0].contentDocument;
-    //var container = iframe.getElementById("namee");
-    var previousCanvas = iframe.getElementsByTagName("canvas")[0];
-    previousCanvas.parentElement.removeChild(previousCanvas);
+    var container = iframe.getElementById("namee");
+
     // CAMERA
 
     camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1500);
@@ -503,8 +502,8 @@ function init() {
     renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setClearColor(scene.fog.color);
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(700, 400);
-    iframe.getElementsByTagName("body")[0].appendChild(renderer.domElement);
+    renderer.setSize(400, 170);
+    container.appendChild(renderer.domElement);
 }
 
 function loadFont() {
