9b429119f50d922b9e73399712162e9bebdf88b0
Aruna Shanmugakumar <arunash@thoughtworks.com>
2017-05-06 05:53:21 +0530

    debugging heroku issue

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 process.env.PWD = process.cwd();
+console.log(__dirname);
+console.log(process.cwd());
 app.use(express.static( __dirname + '/VR_VIEW'));
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
+});
+
+app.get('/', function(req, res){
+    res.sendFile(path.join( __dirname, 'VR_VIEW/website/vrview/examples/hotspots', 'index.html' ));
 });
