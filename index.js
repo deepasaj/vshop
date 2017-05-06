3f1aeb6acf349dddbc7684cb3b0044dd55f093da
Aruna Shanmugakumar <arunash@thoughtworks.com>
2017-05-06 05:43:36 +0530

    Fixing heroku disp

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 process.env.PWD = process.cwd();
 app.use(express.static( __dirname + '/VR_VIEW'));
-app.set('views', path.join(process.env.PWD, 'VR_VIEW'));
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
